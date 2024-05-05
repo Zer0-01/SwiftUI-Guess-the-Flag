4923172d42c38d1c37eb0fe7c44f2dde44d87aed
Anas Zulkifli bin Mohd Jeffry <206520@student.upm.edu.my>
2024-05-05 10:48:42 +0800

    first commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Silentmode Sdn Bhd on 03/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
